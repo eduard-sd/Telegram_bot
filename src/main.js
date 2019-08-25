0caa9992bc8c6bafd54f92bb82bf439b7ce7948b
eduard-sd <pr-spektr@yandex.ru>
2019-08-25 23:51:03 +0300

    one new method

--- src/main.js
+++ src/main.js
@@ -2,19 +2,19 @@ const TelegramBot = require('node-telegram-bot-api');
 const TOKEN = "869793649:AAFpPGkJ1Q7XUMmENONXuzQPmPgktGb7C9A";
 const bot = new TelegramBot(TOKEN, {polling: true});
 const inlineKeyboardMarkup = new InlineKeyboardMarkup();
-
-let keyboard = {
-   inline_keyboard: [
-      {
-         text: 'Google',
-         url: 'http://google.com'
-      },
-      {
-         text: 'Ysndex',
-         url: 'http://yandex.ru'
-      }
-   ]
-}
+//
+// let keyboard = {
+//    inline_keyboard: [
+//       {
+//          text: 'Google',
+//          url: 'http://google.com'
+//       },
+//       {
+//          text: 'Ysndex',
+//          url: 'http://yandex.ru'
+//       }
+//    ]
+// }
 
 
 bot.on("message",msg => {
@@ -22,7 +22,7 @@ bot.on("message",msg => {
    
 });
 
-bot.sendMessage(msg.chat.id,startMessage, keyboard);
+// bot.sendMessage(msg.chat.id,startMessage, keyboard);
 
 // Фронт:
 //
