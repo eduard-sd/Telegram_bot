e6f9b77166b91623daddb0ae34eaa691169a8d02
eduard-sd <pr-spektr@yandex.ru>
2019-08-30 23:24:53 +0300

    Refactor

--- src/main.js
+++ src/main.js
@@ -31,24 +31,24 @@ const bot = new TelegramBot(TOKEN, {polling: true});
 //
 // });
 
-bot.on("message",msg => {
-    let startMessage = 'Приветствие';
-    let keyboard = {
-        reply_markup: {
-            inline_keyboard: [
-                {
-                    text: 'Google',
-                    url: 'http://google.com'
-                },
-                {
-                    text: 'Ysndex',
-                    url: 'http://yandex.ru'
-                }
-            ]
-        }
-    };
-    bot.sendMessage(msg.chat.id,startMessage, keyboard);
-});
+// bot.on("message",msg => {
+//     let startMessage = 'Приветствие';
+//     let keyboard = {
+//         reply_markup: {
+//             inline_keyboard: [
+//                 {
+//                     text: 'Google',
+//                     url: 'http://google.com'
+//                 },
+//                 {
+//                     text: 'Ysndex',
+//                     url: 'http://yandex.ru'
+//                 }
+//             ]
+//         }
+//     };
+//     bot.sendMessage(msg.chat.id,startMessage, keyboard);
+// });
 
 
 // Фронт:
@@ -68,3 +68,26 @@ bot.on("message",msg => {
 //ssh 12345
 
 
+
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+
+    const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
+
+    // send back the matched "whatever" to the chat
+    bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+
+    // send a message to the chat acknowledging receipt of their message
+    bot.sendMessage(chatId, 'Received your message');
+});
