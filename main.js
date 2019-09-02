464f5b8197564f5223710768951b7abc04205127
eduard-sd <pr-spektr@yandex.ru>
2019-09-02 23:08:48 +0300

    переход на yarn

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-process.env.NTBA_FIX_319 = 1;
+import TelegramBot from 'node-telegram-bot-api'
+import config from 'config'
 
-
-const TelegramBot = require('node-telegram-bot-api');
-
-// replace the value below with the Telegram token you receive from @BotFather
-const token = "869793649:AAGBDwsYi_yCpSQ5V7_qvaTUx08pVkOn2GQ";
+const TOKEN = config.get('token');
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(TOKEN, {polling: true});
 
 bot.on('message', (msg) => {
    const chatId = msg.chat.id;
