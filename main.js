f0e6e2d4962e207beb0c069ef2870a080aabf6cb
eduard-sd <pr-spektr@yandex.ru>
2019-09-11 22:59:14 +0300

    Рабочая версия

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
 
 const TelegramBot = require('node-telegram-bot-api');
-const TOKEN = "869793649:AAFpPGkJ1Q7XUMmENONXuzQPmPgktGb7C9A";
-const bot = new TelegramBot(TOKEN, {polling: {interval: 1000}});
+const TOKEN = "869793649:AAGBDwsYi_yCpSQ5V7_qvaTUx08pVkOn2GQ";
+const bot = new TelegramBot(TOKEN, {polling: true});
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
+// bot.onText(/\/echo (.+)/, (msg, match) => {
+//     const chatId = msg.chat.id;
+//     const resp = match[1];
+//     bot.sendMessage(chatId, resp);
+// });
 
-    const chatId = msg.chat.id;
-    const resp = match[1]; // the captured "whatever"
+const keyboard = {
+    reply_markup: {
+        inline_keyboard: [
+            [
+                {
+                    text: "Шары",
+                    url: "www.google.com"
+                },
+                {
+                    text: "Торты",
+                    url: "www.google.com"
+                }
+            ],
+            [
+                {
+                    text: "Торты2",
+                    url: "www.google.com"
+                }
+            ]
+        ]
+    }
+};
 
-    // send back the matched "whatever" to the chat
-    bot.sendMessage(chatId, resp);
-});
 
-// Listen for any kind of message. There are different kinds of
-// messages.
 bot.on('message', (msg) => {
+    const chatValue = msg.chat;
     const chatId = msg.chat.id;
-
-    // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, 'Received your message');
+    const chatOpponent = chatValue.first_name;
+    bot.sendMessage(chatId,
+        `${chatOpponent}, добрый день! Вас приветсвует автоматичекский помошник ввыборе шаров. Прошу сдлеть выбор чем я могу вам помочь.`,
+        keyboard);
 });
